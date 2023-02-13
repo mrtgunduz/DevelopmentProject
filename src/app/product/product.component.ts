cd465e9e5dc94daf14b92d1fe66a27a7bb44640b
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-13 21:11:20 +0300

    add alertift success

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
-import { AlertifyService } from '../services/alertify.service';
-declare var alertify: any;
+import * as alertify from 'alertifyjs'
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  constructor ( ) { }
   title = "Ürün Listesi"
+  filterText = ""
   products:  Product[] = [
     {id: 1, name: "Laptop", price:2500, categoryId:1, description: "Asus Zenbook",imageUrl: "assets/images/watch.png"},
     {id: 2, name: "Mouse", price:25, categoryId:2, description: "A4 Tech",imageUrl: "assets/images/box.png"},
     ];
-    filterText = ""
-
-
-
-
-  constructor ( private alertify: AlertifyService) { }
+   
     ngOnInit(): void {
-    }
+    };
 
-    addToCart(product:any) {
-     this.alertify.success("ssdfsdf " + product.name)
+
+    
+
+    addToCart(product){
+      alertify.success("Ürün sepete eklendi " + product.name)
     }
 
     
