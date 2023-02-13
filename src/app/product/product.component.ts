4efbdca5452970704313b7d024b8d39da92eaabb
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-13 17:23:40 +0300

    fake data

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
+  title = "Ürün Listesi"
+  products: any[] = [
+    {id: 1, name: "Laptop", price:2500, categoryId:1, description: "Asus Zenbook"},
+    {id: 2, name: "Mouse", price:25, categoryId:2, description: "A4 Tech"},
+    ];
+
+
+
+
+  constructor () { }
+    ngOnInit(): void {
+      
+    }
 }
