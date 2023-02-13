93f5b3b9b76392b5b7d7f5d144e3435c06c0aadc
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-13 19:31:08 +0300

    productFilter

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
-
+declare let alertify:any;
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -13,12 +13,17 @@ export class ProductComponent implements OnInit {
     {id: 1, name: "Laptop", price:2500, categoryId:1, description: "Asus Zenbook",imageUrl: "assets/images/watch.png"},
     {id: 2, name: "Mouse", price:25, categoryId:2, description: "A4 Tech",imageUrl: "assets/images/box.png"},
     ];
+    filterText = ""
 
 
 
 
   constructor () { }
     ngOnInit(): void {
-      
+    }
+
+    addToCart(product:any) {
+      alertify.alert('Ready!');
+
     }
 }
