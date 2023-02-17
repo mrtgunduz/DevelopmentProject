6e56f5863c54efd4bd83795e6437ce35f4bac9e3
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-17 16:32:56 +0300

    .

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -6,21 +6,17 @@ import { Product } from '../product/product';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
-
-export class CategoryComponent implements OnInit{
-  constructor(private http:HttpClient) {}
-  title = "Kategori Listesi"
-  categories! : Category[];
-  path = "http://localhost:3000/categories";
-
-
+export class CategoryComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  title = 'Kategori Listesi';
+  categories!: Category[];
+  path = 'http://localhost:3000/categories';
 
   ngOnInit(): void {
-    this.http.get<Category[]>(this.path).subscribe(data=>{
-     this.categories = data
+    this.http.get<Category[]>(this.path).subscribe((data) => {
+      this.categories = data;
     });
-   }
-
+  }
 }
