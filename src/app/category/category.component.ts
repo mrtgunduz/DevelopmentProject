c9e58f985f9722325050cf1bc058d3d19b2701e9
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-17 16:31:59 +0300

    add json category

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Category } from './category';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../product/product';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+
+export class CategoryComponent implements OnInit{
+  constructor(private http:HttpClient) {}
   title = "Kategori Listesi"
-  categories : any;
+  categories! : Category[];
+  path = "http://localhost:3000/categories";
+
+
+
+  ngOnInit(): void {
+    this.http.get<Category[]>(this.path).subscribe(data=>{
+     this.categories = data
+    });
+   }
 
 }
