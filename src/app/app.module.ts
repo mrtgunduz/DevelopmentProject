9798ed3dfe402c85040bf0de7b5759341ea98f06
Murat Gündüz <mrt.gunduz25@gmail.com>
2023-02-17 16:15:22 +0300

    add json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductToastrService } from '../services/product-toastr.service';
 import { Product } from './product';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -8,45 +9,18 @@ import { Product } from './product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  constructor(private toast: ProductToastrService) {}
-  title = 'Ürün Listesi';
+  constructor(private toast: ProductToastrService , private http:HttpClient) {}
+  title = 'Ürün Listesi'
   filterText = '';
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'Test',
-      price: 2500,
-      categoryId: 1,
-      description: 'İphone 6',
-      imageUrl: 'assets/images/ip6.png',
-    },
-    {
-      id: 2,
-      name: 'Best',
-      price: 2600,
-      categoryId: 1,
-      description: 'İphone 7',
-      imageUrl: 'assets/images/ip6.png',
-    },
-    {
-      id: 3,
-      name: 'Kest',
-      price: 2700,
-      categoryId: 1,
-      description: 'İphone 8',
-      imageUrl: 'assets/images/ip6.png',
-    },
-    {
-      id: 4,
-      name: 'Sest',
-      price: 2800,
-      categoryId: 1,
-      description: 'İphone X',
-      imageUrl: 'assets/images/ip6.png',
-    },
-  ];
+  products! : Product[] ;
+  path = "http://localhost:3000/products";
+  
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+   this.http.get<Product[]>(this.path).subscribe(data=>{
+    this.products = data
+   });
+  }
 
 
   sepetEkle(product){
